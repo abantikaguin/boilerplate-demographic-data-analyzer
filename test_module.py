b2c53d1b78a11e2748df8e5bf176794b55a1f9b8
Abantika Guin <abantikaguin23@gmail.com>
2024-12-08 19:49:59 +0530

    test_module.py

--- test_module.py
+++ test_module.py
@@ -3,11 +3,11 @@ import demographic_data_analyzer
 
 class DemographicAnalyzerTestCase(unittest.TestCase):
     @classmethod
-    def setUpClass(self):
-        self.data = demographic_data_analyzer.calculate_demographic_data(print_data = False)
+    def setUpClass(cls):
+        cls.data = demographic_data_analyzer.calculate_demographic_data(print_data=False)
 
     def test_race_count(self):
-        actual = self.data['race_count'].tolist()
+        actual = self.data['race_count']  # Fixed this line
         expected = [27816, 3124, 1039, 311, 271]
         self.assertCountEqual(actual, expected, msg="Expected race count values to be [27816, 3124, 1039, 311, 271]")
 
